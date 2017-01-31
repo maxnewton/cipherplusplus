c79cd7022ba8afa8e523509aef626f00b2a4bdcb
Aaron Powell <waylanddesign@gmail.com>
2017-01-31 00:33:10 -0500

    Initial commit with working code

---
+++ cipher.cpp
@@ -0,0 +1,24 @@
+# include <string>
+
+std::string caesar(std::string text, int shift) {
+	for (std::string::iterator i=text.begin(); i!=text.end(); ++i) {;
+		if (std::isalpha(*i)) {
+			char a;
+			char z;
+			if (islower(*i)) {
+				a = 'a';
+				z = 'z';
+			}
+			else {
+				a = 'A';
+				z = 'Z';
+			}
+
+			int s = (*i - a + shift) % 26;
+
+			if (s < 0) *i = z + (s+1);
+			else *i = a + s;
+		}
+	}
+	return text;
+}
---
+++ cipher.h
@@ -0,0 +1,6 @@
+#ifndef CIPHER_H
+#define CIPHER_H
+
+std::string caesar(std::string text, int shift);
+
+#endif

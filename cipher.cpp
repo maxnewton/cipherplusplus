c4d5e4571b1ee91257aa62baf85282c1c26ab015
Aaron Powell <waylanddesign@gmail.com>
2017-02-01 19:17:51 -0500

    Added Creative Commons License Block to existing files

--- cipher.cpp
+++ cipher.cpp
@@ -1,7 +1,26 @@
+/* ***** CREATIVE COMMONS LICENSE BLOCK *****
+ *
+ * This program is licensed under a Creative Commons Attribution 4.0
+ * International License (CC BY 4.0). You are free to share (copy and
+ * redistribute the material in any medium or format) and adapt (remix,
+ * transform, and build upon the material) for any purpose, even
+ * commercially, as long as you give appropriate credit, provide a link
+ * to the license, and indicate if changes were made. You may do so in
+ * any reasonable manner, but not in any way that suggests the licensor
+ * endorses you or your use.
+ */
+
 # include <string>
 
+std::string atbash(std::string text) {
+	for (std::string::iterator i=text.begin(); i!=text.end(); ++i) {
+		if (std::isalpha(*i)) *i = 'z' - *i;
+	}
+	return text;
+}
+
 std::string caesar(std::string text, int shift) {
-	for (std::string::iterator i=text.begin(); i!=text.end(); ++i) {;
+	for (std::string::iterator i=text.begin(); i!=text.end(); ++i) {
 		if (std::isalpha(*i)) {
 			char a;
 			char z;
--- cipher.h
+++ cipher.h
@@ -1,6 +1,19 @@
+/* ***** CREATIVE COMMONS LICENSE BLOCK *****
+ *
+ * This program is licensed under a Creative Commons Attribution 4.0
+ * International License (CC BY 4.0). You are free to share (copy and
+ * redistribute the material in any medium or format) and adapt (remix,
+ * transform, and build upon the material) for any purpose, even
+ * commercially, as long as you give appropriate credit, provide a link
+ * to the license, and indicate if changes were made. You may do so in
+ * any reasonable manner, but not in any way that suggests the licensor
+ * endorses you or your use.
+ */
+
 #ifndef CIPHER_H
 #define CIPHER_H
 
+std::string atbash(std::string text);
 std::string caesar(std::string text, int shift);
 std::string rot13(std::string text);
 
